e2e2c33ecc976011a3ef02287a1fa6a4585c77ae
haeun92e0 <gracekim8729@gmail.com>
2026-08-14 22:41:28 +0900

    feat : Artist 엔티티와 장르 enum 추가 #19

---
+++ src/main/java/com/greedy/festa/artist/entity/Artist.java
@@ -0,0 +1,47 @@
+package com.greedy.festa.artist.entity;
+
+import com.greedy.festa.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Artist extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private ArtistGenre genre;
+
+    private String imageUrl;
+
+    private String instagramUrl;
+
+    @Column(nullable = false)
+    private boolean needsReview;
+
+    @Builder
+    private Artist(String name, ArtistGenre genre, String imageUrl,
+                   String instagramUrl, boolean needsReview) {
+        this.name = name;
+        this.genre = genre;
+        this.imageUrl = imageUrl;
+        this.instagramUrl = instagramUrl;
+        this.needsReview = needsReview;
+    }
+}
