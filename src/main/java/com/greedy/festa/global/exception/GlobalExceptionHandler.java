7220a3c628edac4432b1859c965cc03898bf485a
htdufhc-bob <htdufhc@gmail.com>
2026-08-14 20:16:45 +0900

    refactor : 전역 예외 처리를 RestControllerAdvice 로 변경 #17

--- src/main/java/com/greedy/festa/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/greedy/festa/global/exception/GlobalExceptionHandler.java
@@ -3,11 +3,11 @@ package com.greedy.festa.global.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(FestaException.class)
