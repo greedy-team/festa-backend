61a785095c313a694435fb05198ae5254daaef31
haeun92e0 <gracekim8729@gmail.com>
2026-09-03 10:42:44 +0900

    fix : LIKE 이스케이프의 null 계약을 명시한다 #98

--- src/main/java/com/greedy/festa/global/util/LikePatternUtils.java
+++ src/main/java/com/greedy/festa/global/util/LikePatternUtils.java
@@ -1,12 +1,16 @@
 package com.greedy.festa.global.util;
 
+import java.util.Objects;
+
 public class LikePatternUtils {
 
     private LikePatternUtils() {
     }
 
+    /** Escapes PostgreSQL LIKE metacharacters so the input is matched literally. */
     public static String escape(String value) {
-        return value.replace("\\", "\\\\")
+        String nonNullValue = Objects.requireNonNull(value, "value must not be null");
+        return nonNullValue.replace("\\", "\\\\")
                 .replace("%", "\\%")
                 .replace("_", "\\_");
     }
--- src/test/java/com/greedy/festa/global/util/LikePatternUtilsTest.java
+++ src/test/java/com/greedy/festa/global/util/LikePatternUtilsTest.java
@@ -3,6 +3,7 @@ package com.greedy.festa.global.util;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNullPointerException;
 
 class LikePatternUtilsTest {
 
@@ -10,5 +11,12 @@ class LikePatternUtilsTest {
     void escapesBackslashBeforeLikeWildcards() {
         assertThat(LikePatternUtils.escape("a\\b%c_d"))
                 .isEqualTo("a\\\\b\\%c\\_d");
+    }
+
+    @Test
+    void rejectsNull() {
+        assertThatNullPointerException()
+                .isThrownBy(() -> LikePatternUtils.escape(null))
+                .withMessage("value must not be null");
     }
 }
